63429b02474cc2da172ed21f19288c631d60d902
Viviana Jorge <viviana.weber@e-unicred.com.br>
2024-05-27 22:42:21 -0300

    Ajuste desafio Busca Nome

--- TrilhaLogicaDesafios/src/buscanome/BuscaNome.java
+++ TrilhaLogicaDesafios/src/buscanome/BuscaNome.java
@@ -8,22 +8,23 @@ public class BuscaNome {
     public static void main(String[] args) {
 
         ArrayList<String> nomes = new ArrayList<>();
-        nomes.add("Ana");
-        nomes.add("Bento");
-        nomes.add("Dani");
-        nomes.add("Caio");
-        nomes.add("Teo");
-        nomes.add("Isis");
-        nomes.add("Yuri");
-        nomes.add("Clara");
-        nomes.add("Elen");
-        nomes.add("Zoe");
+        nomes.add("ANA");
+        nomes.add("BENTO");
+        nomes.add("DANI");
+        nomes.add("CAIO");
+        nomes.add("TEO");
+        nomes.add("ISIS");
+        nomes.add("YURI");
+        nomes.add("CLARA");
+        nomes.add("ELLEN");
+        nomes.add("ZOE");
 
         System.out.println(nomes);
 
         Scanner t = new Scanner(System.in);
         System.out.println("Informe o nome que deseja pesquisar: ");
-        if(nomes.contains(t.next())){
+        String pesquisa = t.next().toUpperCase();
+        if(nomes.contains(pesquisa)){
             System.out.println("Achei");
         } else {
             System.out.println("Não achei");
